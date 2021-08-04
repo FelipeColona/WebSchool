020403756f7fb9dfcff275413cff75785c9a0573
FelipeColona <felipedev202@gmail.com>
2021-08-03 22:04:27 -0300

    feat: create verification whether the teacher teachers a class and home frontend

--- src/main/java/br/com/webschool/api/controller/TeacherController.java
+++ src/main/java/br/com/webschool/api/controller/TeacherController.java
@@ -1,16 +1,29 @@
 package br.com.webschool.api.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.webschool.domain.exception.GeneralException;
+import br.com.webschool.domain.model.Teacher;
+import br.com.webschool.domain.repository.TeacherRepository;
 import lombok.AllArgsConstructor;
 
 @RestController
 @RequestMapping("/teachers")
 @AllArgsConstructor
 public class TeacherController {
+    TeacherRepository teacherRepository;
 
     @Controller
     public class PageRenderer {
@@ -18,10 +31,59 @@ public class TeacherController {
         public String loginPage(){
             return "teachers-login";
         }
+
+        @GetMapping("/teachers")
+        public String home(Model model){
+            Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+    
+            if (principal instanceof UserDetails) {
+                String username = ((Teacher)principal).getName();
+                
+                Teacher currentTeacher = teacherRepository.findByName(username).get();
+                model.addAttribute("classrooms", currentTeacher.getClassrooms());
+                return "teachers-home";
+                
+            } else {
+                throw new GeneralException("ERROR");
+            }
+        }
+
+        @GetMapping("/teachers/{classroomNameUrl}")
+        public String temp(@PathVariable String classroomNameUrl, Model model, HttpServletRequest request){
+            Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+            if(principal instanceof UserDetails){
+                String username = ((Teacher)principal).getName();
+
+                String classroomName = classroomNameUrl.replaceAll("_", " ");
+
+                Teacher currentTeacher = teacherRepository.findByName(username).get();
+                boolean isAllowed = currentTeacher.getClassrooms().stream().anyMatch( classroom -> classroom.getName().equals(classroomName) );
+
+                if(isAllowed){
+                    return "teachers-home";
+                }
+
+                String currentUrl = request.getRequestURL().toString();
+
+                int index = currentUrl.indexOf("/");
+                List<Integer> indexes = new ArrayList<>();
+
+                while (index >= 0) {
+                    indexes.add(index);
+                    index = currentUrl.indexOf("/", index + 1);
+                }
+
+                index = indexes.get(indexes.size() - 1);
+
+                return "redirect:" + request.getRequestURL().delete(index, request.getRequestURL().length()) + "?error";
+                
+            }else{
+                throw new GeneralException("ERROR");
+            }
+        }
     }
 
-    @GetMapping()
-    public String home(){
-        return "Hello World!";
-    }
 }
+
+//System.out.println(request.getRequestURL().toString());
